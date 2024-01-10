0d99c837f7ce806f9a31d022f0ae737805d10051
vrudhsatv <dhruvvats9999@gmail.com>
2024-01-10 15:53:00 +0545

    ctrl repo

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+$(document).ready(function(){
+    $(".service-slider").owlCarousel({
+      items: 1, // Display one item at a time
+      loop: true, // Loop the carousel
+      nav: true, // Display navigation arrows
+      dots: false, // Hide the default dots
+      autoplay: true, // Autoplay the carousel
+      autoplayTimeout: 3000, // Autoplay interval in milliseconds
+      navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"], // Custom navigation text/icons
+    });
+  });
+  
